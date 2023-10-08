6bde59e21adde2b21c5df777829c36814dd92093
Lornavav <>
2023-10-08 22:00:38 +0000

    Add quantity boxes to shopping bag page

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,7 +10,7 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, item_data  in bag.items():
+    for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
@@ -27,7 +27,7 @@ def bag_contents(request):
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
-                    'quantity': item_data,
+                    'quantity': quantity,
                     'product': product,
                     'size': size,
                 })
